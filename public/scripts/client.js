524ecb8f6b37c3941d899830bbb0044dd4c60455
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-15 09:58:25 -0600

    add class to severity p

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,6 +21,7 @@ var displayResults = function(responseObject) {
   if (verbose) console.log('in displayResults', responseObject);
   var $el = $('#appendToDom');
   $el.append('<p id="serverity">' + responseObject.severity + '</p>');
+  $el.children().last().addClass(getSeverityClass(responseObject.severity));
   $el.append('<p id="result">' + responseObject.result + '</p>');
   $el.append('<p id="description">' + responseObject.description + '</p>');
 }; // end displayResults
@@ -49,6 +50,19 @@ var getCodeInfo = function(event) {
   }); // end ajax
 }; // end getCodeInfo
 
+var getSeverityClass = function(severity) {
+  if (verbose) console.log('in getSeverityClass', severity);
+  var className;
+  if (severity === "Keep Driving!") {
+    className = 'green';
+  } else if (severity === 'Be aware. Needs to be inspected.') {
+    className = 'yellow';
+  } else if (severity === 'Stop driving!') {
+    className = 'red';
+  } // end else
+  return className;
+}; // end getSeverityClass
+
 var goHome = function() {
   console.log('in goHome');
   $('.results').hide();
@@ -56,7 +70,7 @@ var goHome = function() {
 }; // end goHome
 
 var showResultDivs = function() {
-  console.log('in showResultDivs');
+  if (verbose) console.log('in showResultDivs');
   $('.results').show();
   $('#codeForm').hide();
   $('#appendToDom').empty();
